358ae883da13b748c59364b20353df1be1954065
JackyBlackson <Jacky_Blackson@outlook.com>
2022-01-25 10:01:31 +0800

    23333

---
+++ Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿
+namespace MySql_Data_Input_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(321, 184);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "MySql Setup";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 30);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(7, 67);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(150, 30);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(7, 104);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(150, 30);
+            this.textBox3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(164, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "User Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(164, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(164, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(148, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Database Name";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(164, 141);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 34);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Quit";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.numericUpDown2);
+            this.groupBox2.Controls.Add(this.numericUpDown1);
+            this.groupBox2.Controls.Add(this.textBox5);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Location = new System.Drawing.Point(341, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(448, 184);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "WebServer Setup";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(6, 30);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(194, 30);
+            this.textBox4.TabIndex = 0;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(7, 68);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(194, 30);
+            this.textBox5.TabIndex = 1;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(7, 105);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(93, 30);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(106, 105);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(94, 30);
+            this.numericUpDown2.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(7, 141);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(194, 34);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Start Server";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.richTextBox1);
+            this.groupBox3.Location = new System.Drawing.Point(796, 13);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(692, 184);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Status";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(6, 27);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(680, 151);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.richTextBox2);
+            this.groupBox4.Location = new System.Drawing.Point(13, 204);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(1475, 650);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Logs";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(208, 141);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(234, 34);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Close Server";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(208, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 24);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "IP Address";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(208, 70);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 24);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Port";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(208, 104);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(228, 24);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "MaxConnection/TimeOut";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(7, 30);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(1462, 614);
+            this.richTextBox2.TabIndex = 0;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1500, 866);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Tcp_Server_LongConnection.cs
@@ -0,0 +1,410 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+
+
+/// <summary>
+/// 总入口方法，一切操作只需要在这个方法进行
+/// </summary>
+public class TcpLongServer
+{
+    public Socket ServerSocket;
+    public int MaxListenAmount;
+    public ServerThreading thisServerThreader;
+
+    public int ListenFreq;
+    public Thread thisThread;
+
+    //类的构造函数
+    public TcpLongServer(string ip, int port, int maxlistenamount, int maxsilenttime)
+    {
+        //初始化成员
+        ServerSocket = null;
+        MaxListenAmount = maxlistenamount;
+        ListenFreq = 100;
+        thisServerThreader = new ServerThreading(ip, port, MaxListenAmount, ListenFreq);
+        thisServerThreader.MaxSilentTime = maxsilenttime;
+        thisThread = new Thread(new ThreadStart(thisServerThreader.Threading));
+    }
+
+    ///////////////成员的Setter///////////////
+    public void setServerSocket(Socket newSocket)
+    {
+        ServerSocket = newSocket;
+    }
+    public void setMaxListenAmount(int amount)
+    {
+        MaxListenAmount = amount;
+    }
+    public void setListenFreq(int freq)
+    {
+        ListenFreq = freq;
+    }
+    ////////////////////////////////////////////////
+
+    public void Start()
+    {
+        thisThread.Start();
+    }
+
+    public void Close()
+    {
+        for(int i = 0; i < MaxListenAmount; i++)
+        {
+            thisServerThreader.AcceptQueue[i].Send(System.Text.Encoding.UTF8.GetBytes("Server closed."));
+            thisServerThreader.AcceptQueue[i].ThisSocket.Close();
+        }
+        thisServerThreader.Server.Close();
+    }
+
+    //发送数据的方法，但是会覆盖当前的缓冲区，并且会立即打包成一个数据包发走
+    public void Send(int connectionIndex, byte[] buffer)
+    {
+        if (thisServerThreader.AcceptQueue[connectionIndex] == null)
+        {
+            throw new Exception("发送的目标未连接，为空");
+        }
+        else if (thisServerThreader.AcceptQueue[connectionIndex].IsConnected == false)
+        {
+            throw new Exception("发送的目标连接已断开");
+        }
+        else if (thisServerThreader.AcceptQueue[connectionIndex].IsWorking == false)
+        {
+            throw new Exception("发送的目标暂停工作");
+        }
+        else
+        {
+            thisServerThreader.AcceptQueue[connectionIndex].Send(buffer);
+        }
+    }
+
+    //接收数据的方法，返回一个byte为单位字节数组，并清除缓冲区
+    public byte[] Receive(int connectionIndex)
+    {
+        byte[] temp = new byte[2048];
+        thisServerThreader.AcceptQueue[connectionIndex].OutBuffer.ReadBuffer(temp, 0, thisServerThreader.AcceptQueue[connectionIndex].OutBuffer.DataCount);
+        thisServerThreader.AcceptQueue[connectionIndex].OutBuffer.Clear(20480);
+        return temp;
+    }
+}
+
+
+
+
+
+
+
+
+
+/// <summary>
+/// 实际负责开启线程和进行各项检测的类
+/// </summary>
+public class ServerThreading
+{
+    private int MaxListenAmount { get; set; }
+    public AcceptedThreadObjective[] AcceptQueue;
+    private int ListenFreq { get; set; }
+    private string Ip { get; set; }
+    private int Port { get; set; }
+
+
+    public Socket Server; //Server的对象
+
+    public bool isConnected;        //是否链接的状态
+
+
+    public long MaxSilentTime { get; set; }
+
+    public ServerThreading(string ip, int port, int maxlstamt, int lstfreq)
+    {
+        Server = null;
+        MaxListenAmount = maxlstamt;
+        AcceptQueue = new AcceptedThreadObjective[maxlstamt];
+        ListenFreq = lstfreq;
+        Ip = ip;
+        Port = port;        
+        isConnected = false;
+    }
+
+    public void Threading()
+    {
+        try     //尝试服务器连接
+        {
+            Server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Server.Bind(new IPEndPoint(IPAddress.Parse(Ip), Port));
+            Server.Listen(MaxListenAmount);
+            isConnected = true;
+            Console.WriteLine("[Server Thread] Server started, wait for connection.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return;
+        }
+
+        //开启Accept的线程
+        Server.BeginAccept(new AsyncCallback(AcceptCallback), Server);
+
+        //检查每个连接是否有效
+        while(isConnected == true)
+        {
+            //尝试监测每个连接是否由客户端主动断开
+            for (int i = 0; i < MaxListenAmount; i++)
+            {
+                if (AcceptQueue[i] != null && AcceptQueue[i].IsConnected == false)
+                {
+                    AcceptQueue[i] = null;
+                    Console.WriteLine("[Accept Thread] 检测到连接[{0}]被客户端主动断开，Socket将被销毁", i);
+                }
+            }
+            //检查每个连接是否无响应
+            for (int i = 0; i < MaxListenAmount; i++)
+            {
+                if (AcceptQueue[i] != null && AcceptQueue[i].IsConnected == true)
+                {
+                    if(DateTime.Now.ToUniversalTime().Second - AcceptQueue[i].LastReceiveTime.Second >= MaxSilentTime || DateTime.Now.ToUniversalTime().Second - AcceptQueue[i].LastReceiveTime.Second < 0)
+                    {
+                        AcceptQueue[i].Send(System.Text.Encoding.UTF8.GetBytes("Client time out. Connection will close."));
+                        AcceptQueue[i].IsWorking = false;
+                        AcceptQueue[i].IsConnected = false;
+                        Console.WriteLine("[Accept Thread] 检测到连接[{0}]无响应，Socket将被销毁", i);
+                        Thread.Sleep(50);
+                        AcceptQueue[i].ThisSocket.Close();
+                        AcceptQueue[i] = null;
+                    }
+                    else
+                    {
+                        Console.WriteLine("[Accept Thread] 连接[{0}]最新相应在{1}秒之前，通过", i, DateTime.Now.ToUniversalTime().Second - AcceptQueue[i].LastReceiveTime.Second);
+                    }
+                }
+            }
+            Thread.Sleep(500);
+        }
+    }
+
+    public void AcceptCallback(IAsyncResult ar)
+    {
+        // Get the socket that handles the client request.
+        Socket listener = (Socket)ar.AsyncState;
+
+        // End the operation and display the received data on the console.
+
+        int queueIndex = 0;
+        //遍历线程池，直到遇到一个空闲的线程
+        try
+        {
+            for (queueIndex = 0; AcceptQueue[queueIndex] != null && queueIndex <= MaxListenAmount; queueIndex++)
+            {
+                Console.WriteLine("[Accept Thread] Searching[{0}/{1}]: [{2}]", queueIndex, MaxListenAmount - 1, AcceptQueue[queueIndex].ThisSocket.RemoteEndPoint);
+            }
+            Console.WriteLine("[Accept Thread] 当前空闲：[{0}/{1}]", queueIndex, MaxListenAmount - 1);
+
+            AcceptQueue[queueIndex] = new AcceptedThreadObjective(listener.EndAccept(ar), ListenFreq);
+            AcceptQueue[queueIndex].ConnectionId = queueIndex;
+            AcceptQueue[queueIndex].Begin();
+            Console.WriteLine("[Accept Thread] 建立了新的连接，id为 {0}，目标为{1}", queueIndex, AcceptQueue[queueIndex].ThisSocket.RemoteEndPoint);
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("[Accept Thread] ERROR！遇到新的连接请求，但所有连接都已经被占用");
+        }
+
+        //开启这个线程的对象
+
+
+        if (isConnected == true)        //如果这个server没有被关闭，那么才继续
+        {
+            //继续Accept
+            listener.BeginAccept(new AsyncCallback(AcceptCallback), listener);
+        }
+    }
+}
+///EOC///
+
+
+
+
+
+
+
+
+
+
+
+/// <summary>
+/// 每个连接的对象的类，负责开启监听和发送的线程
+/// </summary>
+public class AcceptedThreadObjective
+{
+    public Socket ThisSocket { get; set; }   //这个accept的socket
+    public bool IsWorking { get; set; }   //是否在工作  
+    public bool IsConnected { get; set; }   //是否连接存在
+    public int ConnectionId { get; set; }   //连接的id
+    private int ListenFreq { get; set; }    //设置连接的接收频率
+
+    public RingBufferManager OutBuffer;
+
+    public byte[] ReceiveBuffer;    //接收的缓冲区
+    public byte[] SendBuffer;       //发送的缓冲区
+
+    public DateTime LastReceiveTime;
+
+    public AcceptedThreadObjective(Socket socket, int listenfreq)
+    {
+        //对成员进行初始化
+        ThisSocket = socket;
+        IsWorking = false;
+        ListenFreq = listenfreq;
+        ReceiveBuffer = new byte[2048];
+        SendBuffer = new byte[2048];
+        OutBuffer = new RingBufferManager(2048);
+        LastReceiveTime = DateTime.Now.ToUniversalTime();
+    }
+
+    /// <summary>
+    /// 重新开始信息的发送和接收
+    /// </summary>
+    public void Begin()
+    {
+        if (IsWorking == false)
+        {
+            //将发送的状态设置为真
+            IsWorking = true;
+            IsConnected = true;
+
+            //启动接收的线程
+            ThisSocket.BeginReceive(ReceiveBuffer, 0, ReceiveBuffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), ThisSocket);
+
+            //启动发送的线程
+            //ThisSocket.BeginSend(SendBuffer, 0, SendBuffer.Length, SocketFlags.None, new AsyncCallback(SendCallback), ThisSocket);
+            
+        }
+        else if (ThisSocket == null)
+        {
+            throw new Exception("尝试启动一个空的服务端连接");
+        }
+        else if (IsWorking == true)
+        {
+            throw new Exception("尝试启动一个已经开启的服务端连接");
+        }
+        else
+            throw new Exception("在尝试启动连接的时候遇到了未知问题");
+    }
+
+    /// <summary>
+    /// 暂停信息的接收
+    /// </summary>
+    public void Pause()
+    {
+        if (IsWorking == true)
+            IsWorking = false;
+        else if (ThisSocket == null)
+            throw new Exception("尝试关闭一个未建立的服务端连接");
+        else if (IsWorking == false)
+            throw new Exception("尝试关闭一个已经关闭的服务端连接");
+        else
+            throw new Exception("在尝试关闭一个服务端连接时遇到问题");
+    }
+
+    public void Send(byte[] buffer)
+    {
+        if (IsConnected == true)
+        {
+            ThisSocket.BeginSend(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(SendCallback), ThisSocket);
+        }
+        else
+        {
+            throw new Exception("连接已关闭，请之后再试");
+        }
+    }
+
+
+
+    ////////////////// 回调方法 回调方法 回调方法 //////////////////
+    ////////////////// 回调方法 回调方法 回调方法 //////////////////
+    ////////////////// 回调方法 回调方法 回调方法 //////////////////
+
+    /// <summary>
+    ///接收数据的回调方法
+    /// </summary>
+    /// <param name="result">
+    /// 由BegineReceive方法调用返回的 IAsyncResult 类型的对象
+    /// </param>
+    public void ReceiveCallback(IAsyncResult result)
+    {
+        //初始化
+        
+        Socket ts = (Socket)result.AsyncState;
+        if (IsConnected == true && IsWorking == true)
+        {
+            ts.EndReceive(result);
+        }
+        result.AsyncWaitHandle.Close();
+
+        //读取监听，并写入输出缓冲区，并记录
+        //Console.WriteLine("[Server Thread {0}] （前）缓冲区位置：End: {1}, Start: {2}, length: {3}", ConnectionId,  OutBuffer.DataEnd, OutBuffer.DataStart, OutBuffer.DataCount);
+        if (IsConnected == true && IsWorking == true)
+        {
+            Console.WriteLine("[Server Thread {0}] 从{1}收到消息：{2}", ConnectionId, ThisSocket.RemoteEndPoint, System.Text.Encoding.UTF8.GetString(ReceiveBuffer));
+        }
+
+        //获取读取的数据的长度
+        int datalength = 0;
+        for (datalength = 0; ReceiveBuffer[datalength] != 0; datalength++) ;
+        if (datalength == 0)
+        {
+            IsWorking = false;
+            IsConnected = false;
+            Console.WriteLine("[Server Thread {0}] 连接[{0}]已断开", ConnectionId);
+        }
+
+        //写入环形buffer
+        OutBuffer.WriteBuffer(ReceiveBuffer, 0, datalength);
+        //Console.WriteLine("[Server Thread {0}] （后）缓冲区位置：End: {1}, Start: {2}, length: {3}",ConnectionId,  OodutBuffer.DataEnd, OutBuffer.DataStart, OutBuffer.DataCount);
+
+        /*
+        if (SendBuffer[0] != 0)
+        {
+            ts.Send(SendBuffer);
+        }
+        */
+        
+        //如果收到信息，那么就写入最新的时间
+
+        LastReceiveTime = DateTime.Now.ToUniversalTime();
+
+        
+
+        if (IsWorking == true)
+        {
+            //延时
+            Thread.Sleep(ListenFreq);
+            //清空接收缓冲，并开始新的缓冲
+            ReceiveBuffer = new byte[ReceiveBuffer.Length];
+            ts.BeginReceive(ReceiveBuffer, 0, ReceiveBuffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), ts);
+            LastReceiveTime = DateTime.Now.ToUniversalTime();
+        }
+    }
+
+    /// <summary>
+    /// 发送数据的回调方法
+    /// </summary>
+    /// <param name="result">由BegineSend方法调用返回的 IAsyncResult 类型的对象</param>
+    public void SendCallback(IAsyncResult result)
+    {
+        Socket ts = (Socket)result.AsyncState;
+        ts.EndSend(result);
+        result.AsyncWaitHandle.Close();
+        Console.WriteLine("[Server Thread {0}] 发送信息：{1}",ConnectionId, System.Text.Encoding.UTF8.GetString(SendBuffer));
+
+        //清空数据，重新开始异步发送
+        //阻塞，直到缓冲区重新非空
+        SendBuffer = new byte[SendBuffer.Length];
+    }
+}
+
