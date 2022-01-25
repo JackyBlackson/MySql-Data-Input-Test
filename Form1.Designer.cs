ae08c4de9930d921843d650a0bd9ff5103f5cdf1
JackyBlackson <Jacky_Blackson@outlook.com>
2022-01-25 17:03:18 +0800

    23333

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,33 +31,33 @@ namespace MySql_Data_Input_Test
         {
             this.components = new System.ComponentModel.Container();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
-            this.button4 = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.groupBox3.SuspendLayout();
             this.groupBox4.SuspendLayout();
             this.SuspendLayout();
@@ -79,53 +79,14 @@ namespace MySql_Data_Input_Test
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "MySql Setup";
             // 
-            // textBox1
+            // button2
             // 
-            this.textBox1.Location = new System.Drawing.Point(7, 30);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(150, 30);
-            this.textBox1.TabIndex = 0;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(7, 67);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(150, 30);
-            this.textBox2.TabIndex = 1;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(7, 104);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(150, 30);
-            this.textBox3.TabIndex = 2;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(7, 141);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(150, 34);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Login";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(164, 30);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(105, 24);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "User Name";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(164, 67);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(91, 24);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Password";
+            this.button2.Location = new System.Drawing.Point(164, 141);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 34);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Quit";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // label3
             // 
@@ -136,14 +97,53 @@ namespace MySql_Data_Input_Test
             this.label3.TabIndex = 6;
             this.label3.Text = "Database Name";
             // 
-            // button2
+            // label2
             // 
-            this.button2.Location = new System.Drawing.Point(164, 141);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(148, 34);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "Quit";
-            this.button2.UseVisualStyleBackColor = true;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(164, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(164, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "User Name";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(7, 104);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(150, 30);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(7, 67);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(150, 30);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 30);
+            this.textBox1.TabIndex = 0;
             // 
             // groupBox2
             // 
@@ -163,33 +163,41 @@ namespace MySql_Data_Input_Test
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "WebServer Setup";
             // 
-            // textBox4
+            // label6
             // 
-            this.textBox4.Location = new System.Drawing.Point(6, 30);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(194, 30);
-            this.textBox4.TabIndex = 0;
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(208, 104);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(228, 24);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "MaxConnection/TimeOut";
             // 
-            // textBox5
+            // label5
             // 
-            this.textBox5.Location = new System.Drawing.Point(7, 68);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(194, 30);
-            this.textBox5.TabIndex = 1;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(208, 70);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 24);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Port";
             // 
-            // numericUpDown1
+            // label4
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(7, 105);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(93, 30);
-            this.numericUpDown1.TabIndex = 2;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(208, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 24);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "IP Address";
             // 
-            // numericUpDown2
+            // button4
             // 
-            this.numericUpDown2.Location = new System.Drawing.Point(106, 105);
-            this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(94, 30);
-            this.numericUpDown2.TabIndex = 3;
+            this.button4.Location = new System.Drawing.Point(208, 141);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(234, 34);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Close Server";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
@@ -199,6 +207,35 @@ namespace MySql_Data_Input_Test
             this.button3.TabIndex = 4;
             this.button3.Text = "Start Server";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(106, 105);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(94, 30);
+            this.numericUpDown2.TabIndex = 3;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(7, 105);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(93, 30);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(7, 68);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(194, 30);
+            this.textBox5.TabIndex = 1;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(6, 30);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(194, 30);
+            this.textBox4.TabIndex = 0;
             // 
             // groupBox3
             // 
@@ -228,42 +265,6 @@ namespace MySql_Data_Input_Test
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Logs";
             // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(208, 141);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(234, 34);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "Close Server";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(208, 30);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(101, 24);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "IP Address";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(208, 70);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(46, 24);
-            this.label5.TabIndex = 7;
-            this.label5.Text = "Port";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(208, 104);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(228, 24);
-            this.label6.TabIndex = 8;
-            this.label6.Text = "MaxConnection/TimeOut";
-            // 
             // richTextBox2
             // 
             this.richTextBox2.Location = new System.Drawing.Point(7, 30);
@@ -283,12 +284,13 @@ namespace MySql_Data_Input_Test
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.groupBox3.ResumeLayout(false);
             this.groupBox4.ResumeLayout(false);
             this.ResumeLayout(false);
--- Form1.cs
+++ Form1.cs
@@ -12,10 +12,24 @@ namespace MySql_Data_Input_Test
 {
     public partial class Form1 : Form
     {
+        public TcpLongServer Server;
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Server = new TcpLongServer(textBox4.Text, int.Parse(textBox5.Text), (int)numericUpDown1.Value, (int)numericUpDown1.Value);
+                Server.Start();
+                richTextBox2.AppendText("[Server Starter] 服务器已启动");
+            }
+            catch(Exception ex)
+            {
+                richTextBox2.AppendText("[Server Starter] " + ex.Message + "\n");
+            }
+        }
     }
 }
