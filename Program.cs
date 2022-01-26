4c18f0ad17c0d4fe253378ab2e5df37ea002b5c2
JackyBlackson <Jacky_Blackson@outlook.com>
2022-01-26 16:29:48 +0800

    2333

--- Program.cs
+++ Program.cs
@@ -1,23 +1,56 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
-namespace MySql_Data_Input_Test
+namespace TCP_Connection
 {
-    static class Program
+    public class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            /*
+            string[] input;
+            Console.WriteLine("Hello, please connect me!");
+            Console.WriteLine("Please input info as \"ip:port:maxConnectionAmount\"");
+            input = Console.ReadLine().ToString().Split(':');
+            */
+
+
+            //TcpLongServer thisServer = new TcpLongServer(input[0], int.Parse(input[1]), int.Parse(input[2]));
+
+            byte[] HeartBeatFrame = new byte[2048];
+            TcpLongServer thisServer = new TcpLongServer("192.168.3.85", 2000, 100, 30);
+            thisServer.Start();
+
+            Console.ReadLine();
+            while (true)
+            {
+                //try
+                {
+                    /*
+                    string[] send = new string[2];
+                    send = Console.ReadLine().Split(':');
+                    thisServer.Send(int.Parse(send[0]), System.Text.Encoding.UTF8.GetBytes(send[1]));
+                    */
+                    thisServer.Receive(0);
+                    thisServer.Receive(1);
+                    Thread.Sleep(1000);
+                }
+                /*
+                catch (Exception)
+                {
+                    Console.WriteLine("[ Main Thread ] 输入格式错误，请按照“连接编号:内容的格式来输入！”");
+                }
+                */
+            }
         }
     }
+    //End of Class
 }
+
+
+
---
+++ README.md
@@ -0,0 +1,9 @@
+# .Net-TCP-LongConnection-Manager
+>_&lt;
+## Hello
+## Thank you!
+## Goodbye
+
+## 你好
+## 小笼包
+## 再见
